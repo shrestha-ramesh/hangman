fd3191d9e66960c7669c9a2dc6f81fa8fb16ce41
Ramesh Shrestha <shresthashare@gmail.com>
2020-11-30 14:43:48 -0600

    getPuzzle function to show and hide word

--- hangman.js
+++ hangman.js
@@ -1,6 +1,21 @@
 const Hangman = function(word, remainingGuesses){
-    this.word = word;
+    this.word = word.toLowerCase().split('');
     this. remainingGuesses = remainingGuesses;
+    this.guessLetters = ['c','n']
+}
+Hangman.prototype.getPuzzle=function(){
+    let puzzle = '';
+    this.word.forEach((letter)=>{
+        if(this.guessLetters.includes(letter) || letter === ' '){
+            puzzle += letter;
+        }else{
+            puzzle += '*'
+        }
+    })
+    return puzzle;
 }
 const game1 = new Hangman('Cat', 2)
-console.log(game1)
+console.log(game1.getPuzzle())
+
+const game2 = new Hangman('New Jersey', 4)
+console.log(game2.getPuzzle())
